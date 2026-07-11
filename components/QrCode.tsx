c091ae0faf0de7afab2591fadffb351180fb78e8
irfpannn <irfpann@gmail.com>
2026-07-11 23:07:01 +0800

    feat: add QrCode component using react-qr-code library

--- components/QrCode.tsx
+++ components/QrCode.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { QRCodeSVG } from 'qrcode.react';
+import QRCode from 'react-qr-code';
 
 interface QrCodeProps {
   value: string;
@@ -10,12 +10,12 @@ interface QrCodeProps {
 
 export default function QrCode({ value, size = 180 }: QrCodeProps) {
   return (
-    <div className="bg-white p-3 rounded-2xl flex items-center justify-center shadow-inner select-none">
-      <QRCodeSVG
+    <div className='bg-white p-3 rounded-2xl flex items-center justify-center shadow-inner select-none w-full max-w-[190px] mx-auto aspect-square'>
+      <QRCode
         value={value}
         size={size}
-        level="M" // Medium error correction level for standard density
-        includeMargin={true}
+        level='L' // Medium error correction level for standard density
+        style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
       />
     </div>
   );
